f4f92e28333a8505627f18b5a735d65e3c086af0
= <robalex@gmail.com>
2025-07-09 12:54:53 -0600

    Commit container code. Add tests.

--- Container/Container.cs
+++ Container/Container.cs
@@ -1,10 +1,46 @@
 using System;
+using System.Collections.Generic;
 
 namespace DeveloperSample.Container
 {
     public class Container
     {
-        public void Bind(Type interfaceType, Type implementationType) => throw new NotImplementedException();
-        public T Get<T>() => throw new NotImplementedException();
+        private Dictionary<Type, Type> _interfaceToImplementationMap = [];
+
+        public void Bind(Type interfaceType, Type implementationType)
+        {
+            ValidateBindArguments(interfaceType, implementationType);
+
+            _interfaceToImplementationMap[interfaceType] = implementationType;
+        }
+
+        public T Get<T>()
+        {
+            var interfaceType = typeof(T);
+
+            if (!_interfaceToImplementationMap.TryGetValue(interfaceType, out Type value))
+            {
+                throw new InvalidOperationException();
+            }
+
+            return (T)Activator.CreateInstance(value);
+        }
+
+        private void ValidateBindArguments(Type interfaceType, Type implementationType)
+        {
+            ArgumentNullException.ThrowIfNull(interfaceType);
+
+            ArgumentNullException.ThrowIfNull(implementationType);
+
+            if (!implementationType.IsAssignableTo(interfaceType))
+            {
+                throw new ArgumentException("implementationType is not assignable to interfaceType");
+            }
+
+            if (_interfaceToImplementationMap.ContainsKey(interfaceType))
+            {
+                throw new ArgumentException("interfaceType already bound to an implementation");
+            }
+        }
     }
 }
--- Container/ContainerTest.cs
+++ Container/ContainerTest.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace DeveloperSample.Container
@@ -10,15 +12,55 @@ namespace DeveloperSample.Container
     {
     }
 
+    internal class SecondContainerTestClass : IContainerTestInterface
+    {
+    }
+
     public class ContainerTest
     {
-        [Fact(Skip="Not implemented")]
+        [Fact]
         public void CanBindAndGetService()
         {
             var container = new Container();
             container.Bind(typeof(IContainerTestInterface), typeof(ContainerTestClass));
             var testInstance = container.Get<IContainerTestInterface>();
             Assert.IsType<ContainerTestClass>(testInstance);
+        }
+
+        [Fact]
+        public void ImplementationNotBound()
+        {
+            var container = new Container();
+            Assert.Throws<InvalidOperationException>(() => container.Get<IContainerTestInterface>());
+        }
+
+        [Fact]
+        public void CannotDoubleBindInterface()
+        {
+            var container = new Container();
+            container.Bind(typeof(IContainerTestInterface), typeof(ContainerTestClass));
+            Assert.Throws<ArgumentException>(() => container.Bind(typeof(IContainerTestInterface), typeof(SecondContainerTestClass)));
+        }
+
+        [Fact]
+        public void ImplementationDoesNotImplementInterface()
+        {
+            var container = new Container();
+            Assert.Throws<ArgumentException>(() => container.Bind(typeof(IContainerTestInterface), typeof(List<string>)));
+        }
+
+        [Fact]
+        public void InterfaceTypeCannotBeNull()
+        {
+            var container = new Container();
+            Assert.Throws<ArgumentNullException>(() => container.Bind(null, typeof(ContainerTestClass)));
+        }
+
+        [Fact]
+        public void ImplementationTypeCannotBeNull()
+        {
+            var container = new Container();
+            Assert.Throws<ArgumentNullException>(() => container.Bind(typeof(IContainerTestInterface), null));
         }
     }
 }
