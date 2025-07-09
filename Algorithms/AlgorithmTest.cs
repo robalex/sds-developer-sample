7bc9bd822bd5abd27970d7efde5156c9c02c3810
= <robalex@gmail.com>
2025-07-09 11:43:15 -0600

    implement algorithms and add some tests

--- Algorithms/AlgorithmTest.cs
+++ Algorithms/AlgorithmTest.cs
@@ -4,16 +4,64 @@ namespace DeveloperSample.Algorithms
 {
     public class AlgorithmTest
     {
-        [Fact(Skip="Not implemented")]
+        [Fact]
         public void CanGetFactorial()
         {
             Assert.Equal(24, Algorithms.GetFactorial(4));
         }
 
-        [Fact(Skip="Not implemented")]
+        [Fact]
+        public void NegativeInputReturnsNegativeOne()
+        {
+            Assert.Equal(-1, Algorithms.GetFactorial(-4));
+        }
+
+        [Fact]
+        public void ZeroInputReturnsOne()
+        {
+            Assert.Equal(1, Algorithms.GetFactorial(0));
+        }
+
+        [Fact]
+        public void LargeValueThrowsOverflowException()
+        {
+            Assert.Throws<System.OverflowException>(() => Algorithms.GetFactorial(int.MaxValue));
+        }
+
+        [Fact]
         public void CanFormatSeparators()
         {
             Assert.Equal("a, b and c", Algorithms.FormatSeparators("a", "b", "c"));
+        }
+
+        [Fact]
+        public void CanFormatTwoItems()
+        {
+            Assert.Equal("a and b", Algorithms.FormatSeparators("a", "b"));
+        }
+
+        [Fact]
+        public void CanFormatOneItem()
+        {
+            Assert.Equal("a", Algorithms.FormatSeparators("a"));
+        }
+
+        [Fact]
+        public void CannotFormatZeroArguments()
+        {
+            Assert.Equal(string.Empty, Algorithms.FormatSeparators());
+        }
+
+        [Fact]
+        public void CannotFormatNull()
+        {
+            Assert.Equal(string.Empty, Algorithms.FormatSeparators(null));
+        }
+
+        [Fact]
+        public void CanFormatManyItems()
+        {
+            Assert.Equal("a, b, c, d, e, f, g, h, i, j and k", Algorithms.FormatSeparators("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"));
         }
     }
 }
--- Algorithms/Algorithms.cs
+++ Algorithms/Algorithms.cs
@@ -4,8 +4,45 @@ namespace DeveloperSample.Algorithms
 {
     public static class Algorithms
     {
-        public static int GetFactorial(int n) => throw new NotImplementedException();
+        public static int GetFactorial(int n)
+        {
+            if (n < 0)
+            {
+                return -1;
+            }
 
-        public static string FormatSeparators(params string[] items) => throw new NotImplementedException();
+            var result = 1;
+            for (var i = n; i > 1; i--)
+            {
+                // Always throws System.OverflowException. If this needs to be performance critical or we want to throw a custom exception, manually check overflow.
+                checked
+                {
+                    result *= i;
+                }
+            }
+
+            return result;
+        }
+
+        public static string FormatSeparators(params string[] items)
+        {
+            if (items == null || items.Length == 0)
+            {
+                return string.Empty;
+            }
+            
+            if (items.Length == 1)
+            {
+                return items[0];
+            }
+            
+            if (items.Length == 2)
+            {
+                return $"{items[0]} and {items[1]}";
+            }
+            
+            var allButLast = string.Join(", ", items, 0, items.Length - 1);
+            return $"{allButLast} and {items[^1]}";
+        }
     }
 }
